91431f42424dff513b5b6f11092f87f99505cad8
Sky <wilsnack44121@gotoltc.edu>
2021-11-15 16:59:08 -0600

    Added Warranty Model

---
+++ StuffInc/Models/Warranty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StuffInc.Models
+{
+    public class Warranty
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string ImageURL { get; set; }
+        public string Description { get; set; }
+    }
+}
