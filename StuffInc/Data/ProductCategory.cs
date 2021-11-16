78c0ac7e3c866276b063e00356d5f2dd02b5af9e
Sky <wilsnack44121@gotoltc.edu>
2021-11-15 18:37:49 -0600

    Added Product Model and PCategory Data

---
+++ StuffInc/Data/ProductCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StuffInc.Data
+{
+    public enum ProductCategory
+    {
+        Food = 1,
+        Clothing,
+        Electronics
+    }
+}
---
+++ StuffInc/Models/Products.cs
@@ -0,0 +1,22 @@
+﻿using StuffInc.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StuffInc.Models
+{
+    public class Products
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string ImageURL { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public DateTime Added { get; set; }
+        public ProductCategory ProductCategory { get; set; }
+    }
+}
