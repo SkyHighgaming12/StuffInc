cd47663f1267c8c1f85ff3f4c2e204056a1834c4
Sky <wilsnack44121@gotoltc.edu>
2021-11-16 16:47:15 -0600

    25 added product view

--- StuffInc/Controllers/ProductController.cs
+++ StuffInc/Controllers/ProductController.cs
@@ -20,7 +20,7 @@ namespace StuffInc.Controllers
         public async Task<IActionResult> Index()
         {
             var allProducts = await _context.Suppliers.ToListAsync();
-            return View();
+            return View(allProducts);
         }
     }
 }
