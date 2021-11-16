a574eedee59362a05d49506981d583607886cdb5
Sky <wilsnack44121@gotoltc.edu>
2021-11-15 18:32:50 -0600

    Added Supplier Model

---
+++ StuffInc/Models/Supplier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StuffInc.Models
+{
+    public class Supplier
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Logo { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
