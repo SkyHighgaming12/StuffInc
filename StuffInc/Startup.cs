782455e06f2876d94a2bc96880b9fe5e70c78019
Sky <wilsnack44121@gotoltc.edu>
2021-11-15 19:11:45 -0600

    14. Configured Sql Server

--- StuffInc/Startup.cs
+++ StuffInc/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +26,7 @@ namespace StuffInc
         public void ConfigureServices(IServiceCollection services)
         {
             //dbcontext config
-            services.AddDbContext<AppDbContext>();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
 
             services.AddControllersWithViews();
         }
