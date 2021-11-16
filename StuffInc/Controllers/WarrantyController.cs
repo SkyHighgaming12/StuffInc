ee6b493a6b718ee720ac47b7f233075f3847bd2a
Sky <wilsnack44121@gotoltc.edu>
2021-11-15 20:55:03 -0600

    19. Added Warranty Controller

---
+++ StuffInc/Controllers/WarrantyController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StuffInc.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StuffInc.Controllers
+{
+    public class WarrantyController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public WarrantyController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allWarranties = await  _context.Warranties.ToListAsync();
+            return View();
+        }
+    }
+}
