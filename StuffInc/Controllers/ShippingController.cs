d52885224715816f9e9f821cd7f2a42a954dae92
Sky <wilsnack44121@gotoltc.edu>
2021-11-15 20:52:25 -0600

    18. Added Shipping Controller

---
+++ StuffInc/Controllers/ShippingController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StuffInc.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StuffInc.Controllers
+{
+    public class ShippingController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ShippingController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _context.Shippings.ToList();
+            return View();
+        }
+    }
+}
