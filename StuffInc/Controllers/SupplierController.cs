f2469f1851f5d56220431efe8eb9654024c7921c
Sky <wilsnack44121@gotoltc.edu>
2021-11-15 20:56:55 -0600

    20. Added Suppliers Controller

---
+++ StuffInc/Controllers/SupplierController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StuffInc.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StuffInc.Controllers
+{
+    public class SupplierController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SupplierController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allSuppliers = await _context.Suppliers.ToListAsync();
+            return View();
+        }
+    }
+}
