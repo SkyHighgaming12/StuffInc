7127a3a6331b2340d25bb1b4fb4b3141bdf899db
Sky <wilsnack44121@gotoltc.edu>
2021-11-17 13:27:28 -0600

    28. Added IShippingSefvice

---
+++ StuffInc/Data/Services/IShippingsService.cs
@@ -0,0 +1,17 @@
+﻿using StuffInc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StuffInc.Data.Services
+{
+    public interface IShippingsService
+    {
+        IEnumerable<Shipping> GetAll();
+        Shipping GetById(int id);
+        void Add(Shipping shipping);
+        Shipping Update(int id, Shipping newShipping);
+        void Delete(int id);
+    }
+}
