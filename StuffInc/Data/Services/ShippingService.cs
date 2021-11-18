ab2cb67e9f7238125bb197ce808b344c449d454d
Sky <wilsnack44121@gotoltc.edu>
2021-11-17 21:12:12 -0600

    38. Added Get all func

--- StuffInc/Data/Services/ShippingService.cs
+++ StuffInc/Data/Services/ShippingService.cs
@@ -28,17 +28,7 @@ namespace StuffInc.Data.Services
             await _context.SaveChangesAsync();
         }
 
-        public async Task<IEnumerable<Shipping>> GetAllAsync()
-        {
-            var result = await _context.Shippings.ToListAsync();
-            return result;
-        }
-
-        public async Task<Shipping> GetByIdAsync(int id)
-        {
-            var result = await _context.Shippings.FirstOrDefaultAsync(n => n.Id == id);
-            return result;
-        }
+        
 
         public async Task<Shipping> UpdateAsync(int id, Shipping newShipping)
         {
