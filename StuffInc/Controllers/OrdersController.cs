b60c4653778df0416f202e69ce8d6bde08c6fdbf
Sky <wilsnack44121@gotoltc.edu>
2021-11-18 20:29:56 -0600

    67. Added orders controller

---
+++ StuffInc/Controllers/OrdersController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StuffInc.Data.Cart;
+using StuffInc.Data.Services;
+using StuffInc.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StuffInc.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IProductsService _productsService;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrdersController(IProductsService productsService, ShoppingCart shoppingCart)
+        {
+            _productsService = productsService;
+            _shoppingCart = shoppingCart;
+        }
+
+
+        public IActionResult Index()
+        {
+
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var response = new ShoppingCartVm()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(response);
+        }
+    }
+}
---
+++ StuffInc/Data/ViewModels/ShoppingCartVm.cs
@@ -0,0 +1,14 @@
+﻿using StuffInc.Data.Cart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StuffInc.Data.ViewModels
+{
+    public class ShoppingCartVm
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public double ShoppingCartTotal { get; set; }
+    }
+}
