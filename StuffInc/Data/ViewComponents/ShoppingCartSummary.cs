b9ebbc46d471acad7e31df53b03be3afd0b511c7
Sky <wilsnack44121@gotoltc.edu>
2021-11-18 21:22:41 -0600

    72. Added a cute lil basket

---
+++ StuffInc/Data/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StuffInc.Data.Cart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StuffInc.Data.ViewComponents
+{
+    public class ShoppingCartSummary:ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+
+            return View(items.Count);
+        }
+    }
+}
