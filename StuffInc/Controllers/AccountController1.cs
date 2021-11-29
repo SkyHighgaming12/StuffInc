f61e5f4a255d5edd02745f3ef7f0143188590aa1
Sky <wilsnack44121@gotoltc.edu>
2021-11-29 14:13:34 -0600

    85. Account Controller

---
+++ StuffInc/Controllers/AccountController1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StuffInc.Data;
+using StuffInc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StuffInc.Controllers
+{
+    public class AccountController1 : Controller
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly AppDbContext _context;
+
+        public AccountController1(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, AppDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
