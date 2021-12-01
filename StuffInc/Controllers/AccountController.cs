2ad02725f98f6efd9459638d5d0030ead85b6278
Sky <wilsnack44121@gotoltc.edu>
2021-12-01 11:08:20 -0600

    87. added login button

---
+++ StuffInc/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StuffInc.Data;
+using StuffInc.Data.ViewModels;
+using StuffInc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StuffInc.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly AppDbContext _context;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, AppDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            var response = new LoginVm();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVm loginVm)
+        {
+            if (!ModelState.IsValid) return View(loginVm);
+            var user = await _userManager.FindByEmailAsync(loginVm.EmailAddress);
+            if(user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVm.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVm.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                TempData["Error"] = "Wrong Email or password, Please Try Again";
+                return View(loginVm);
+            }
+
+            TempData["Error"] = "Wrong Email or password, Please Try Again";
+            return View(loginVm);
+        }
+
+
+
+
+    }
+}
---
+++ StuffInc/Data/ViewModels/LoginVm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StuffInc.Data.ViewModels
+{
+    public class LoginVm
+    {
+
+        [Required(ErrorMessage = "Email Is Required")]
+        [Display(Name = "EmailAddress")]
+        public string EmailAddress { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
